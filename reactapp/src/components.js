d10ab642132b718b0c70df2f90fe82f0e66a7369
Alp Sayin <alpsayin@gmail.com>
2020-10-18 21:06:57 +0300

    initial commit for react app without the actual source

---
+++ reactapp/src/components.js
@@ -0,0 +1,242 @@
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
+import { timeFormat, timeParse } from "d3-time-format";
+
+const human_datetime_formatter = timeFormat("%d-%b");
+
+// *******************************************************
+// RAIL
+// *******************************************************
+const railOuterStyle = {
+  position: 'absolute',
+  width: '100%',
+  height: 42,
+  transform: 'translate(0%, -50%)',
+  borderRadius: 7,
+  cursor: 'pointer',
+  // border: '1px solid white',
+}
+
+const railInnerStyle = {
+  position: 'absolute',
+  width: '100%',
+  height: 14,
+  transform: 'translate(0%, -50%)',
+  borderRadius: 7,
+  pointerEvents: 'none',
+  backgroundColor: 'rgb(155,155,155)',
+}
+
+export function SliderRail({ getRailProps }) {
+  return (
+    <Fragment>
+      <div style={railOuterStyle} {...getRailProps()} />
+      <div style={railInnerStyle} />
+    </Fragment>
+  )
+}
+
+SliderRail.propTypes = {
+  getRailProps: PropTypes.func.isRequired,
+}
+
+// *******************************************************
+// HANDLE COMPONENT
+// *******************************************************
+export function Handle({
+  domain: [min, max],
+  handle: { id, value, percent },
+  disabled,
+  getHandleProps,
+}) {
+  return (
+    <Fragment>
+      <div
+        style={{
+          left: `${percent}%`,
+          position: 'absolute',
+          transform: 'translate(-50%, -50%)',
+          WebkitTapHighlightColor: 'rgba(0,0,0,0)',
+          zIndex: 5,
+          width: 28,
+          height: 42,
+          cursor: 'pointer',
+          // border: '1px solid white',
+          backgroundColor: 'none',
+        }}
+        {...getHandleProps(id)}
+      >
+        <div className={"datehandle"+id.substring(id.indexOf('-')+1)}>
+          {human_datetime_formatter(new Date(value))}
+        </div>
+      </div>
+      <div
+        role="slider"
+        aria-valuemin={min}
+        aria-valuemax={max}
+        aria-valuenow={value}
+        style={{
+          left: `${percent}%`,
+          position: 'absolute',
+          transform: 'translate(-50%, -50%)',
+          zIndex: 2,
+          width: 24,
+          height: 24,
+          borderRadius: '50%',
+          boxShadow: '1px 1px 1px 1px rgba(0, 0, 0, 0.3)',
+          backgroundColor: disabled ? '#666' : '#808F94',
+        }}
+      />
+    </Fragment>
+  )
+}
+
+Handle.propTypes = {
+  domain: PropTypes.array.isRequired,
+  handle: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    value: PropTypes.number.isRequired,
+    percent: PropTypes.number.isRequired,
+  }).isRequired,
+  getHandleProps: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+}
+
+Handle.defaultProps = {
+  disabled: false,
+}
+
+// *******************************************************
+// KEYBOARD HANDLE COMPONENT
+// Uses a button to allow keyboard events
+// *******************************************************
+export function KeyboardHandle({
+  domain: [min, max],
+  handle: { id, value, percent },
+  disabled,
+  getHandleProps,
+}) {
+  return (
+    <button
+      role="slider"
+      aria-valuemin={min}
+      aria-valuemax={max}
+      aria-valuenow={value}
+      style={{
+        left: `${percent}%`,
+        position: 'absolute',
+        transform: 'translate(-50%, -50%)',
+        zIndex: 2,
+        width: 24,
+        height: 24,
+        borderRadius: '50%',
+        boxShadow: '1px 1px 1px 1px rgba(0, 0, 0, 0.3)',
+        backgroundColor: disabled ? '#666' : '#003462',
+      }}
+      {...getHandleProps(id)}
+    />
+  )
+}
+
+KeyboardHandle.propTypes = {
+  domain: PropTypes.array.isRequired,
+  handle: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    value: PropTypes.number.isRequired,
+    percent: PropTypes.number.isRequired,
+  }).isRequired,
+  getHandleProps: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+}
+
+KeyboardHandle.defaultProps = {
+  disabled: false,
+}
+
+// *******************************************************
+// TRACK COMPONENT
+// *******************************************************
+export function Track({ source, target, getTrackProps, disabled }) {
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        transform: 'translate(0%, -50%)',
+        height: 14,
+        zIndex: 1,
+        backgroundColor: disabled ? '#999' : '#7390AA',
+        borderRadius: 7,
+        cursor: 'pointer',
+        left: `${source.percent}%`,
+        width: `${target.percent - source.percent}%`,
+      }}
+      {...getTrackProps()}
+    />
+  )
+}
+
+Track.propTypes = {
+  source: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    value: PropTypes.number.isRequired,
+    percent: PropTypes.number.isRequired,
+  }).isRequired,
+  target: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    value: PropTypes.number.isRequired,
+    percent: PropTypes.number.isRequired,
+  }).isRequired,
+  getTrackProps: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+}
+
+Track.defaultProps = {
+  disabled: false,
+}
+
+// *******************************************************
+// TICK COMPONENT
+// *******************************************************
+export function Tick({ tick, count, format }) {
+  return (
+    <div>
+      <div
+        style={{
+          position: 'absolute',
+          marginTop: 14,
+          width: 2,
+          height: 5,
+          backgroundColor: 'rgb(200,200,200)',
+          left: `${tick.percent}%`,
+        }}
+      />
+      <div
+        style={{
+          position: 'absolute',
+          marginTop: 22,
+          fontSize: 10,
+          textAlign: 'classNamenter',
+          marginLeft: `${-(100 / count) / 2}%`,
+          width: `${100 / count}%`,
+          left: `${tick.percent}%`,
+        }}
+      >
+        <span style={{marginLeft:40}}>{format(tick.value)}</span>
+      </div>
+    </div>
+  )
+}
+
+Tick.propTypes = {
+  tick: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    value: PropTypes.number.isRequired,
+    percent: PropTypes.number.isRequired,
+  }).isRequired,
+  count: PropTypes.number.isRequired,
+  format: PropTypes.func.isRequired,
+}
+
+Tick.defaultProps = {
+  format: d => d,
+}
